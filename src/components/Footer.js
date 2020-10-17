b7dc653b767b40114e94a3c53d177a1df0211644
ianseberg <daniele.coassin@gmail.com>
2020-10-17 23:21:11 +0200

    v2.0.2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ class Footer extends React.Component {
                         <li><a href="https://facebook.com/comprorolascari" target="_blank" className="icon fa-facebook"><span className="label">Twitter</span></a></li>
                         <li><a href="https://g.page/comproorolascari?gm" target="_blank" className="icon fa-google"><span className="label">Github</span></a></li>
                         <li><a href="https://api.whatsapp.com/send?phone=393339253880" target="_blank" className="icon fa-whatsapp "><span className="label">Whatsapp</span></a></li>
-                        <li><a href="https://m.me/comprorolascari" target="_blank" className="icon fa-envelope-o "><span className="label">Facebook Messenger</span></a></li>
+                        <li><a href="https://m.me/comprorolascari" target="_blank" className="icon fa-facebook-square "><span className="label">Facebook Messenger</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; Compro Oro Lascari • <a href="/privacypolicy">Privacy Policy</a></li><li>Design: <a href="https://1000sunny.social">1000 Sunny</a></li>
